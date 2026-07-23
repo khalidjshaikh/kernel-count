dcb2830e75dbbf7d85155e61729490c960fbdd08
Khalid Shaikh <k@ruby.run>
2026-07-22 23:13:47 -0500

    Switch kernel to float32 and add no-AtomCode rule

--- main.go
+++ main.go
@@ -53,15 +53,15 @@ import (
 )
 
 const kernelSource = `
-__kernel void count(__global ulong *output, ulong iterations) {
-    ulong id = get_global_id(0);
-    ulong sum = id;
-    for (ulong i = 0; i < iterations; i++) {
-        sum += (i * 7UL) ^ (i >> 3UL);
-        sum ^= (sum >> 17UL);
-        sum += (sum << 11UL);
+__kernel void count(__global float *output, float iterations) {
+    float id = get_global_id(0);
+    float sum = id;
+    for (float i = 0; i < iterations; i += 1.0f) {
+        sum += (i * 7.0f) + (i / 8.0f);
+        sum += (sum / 131072.0f);
+        sum *= 2048.0f;
     }
-    output[id] = sum;
+    output[(int)id] = sum;
 }
 `
 
@@ -140,11 +140,11 @@ func main() {
 	iterations := uint64(1 << 10) // 1024 per work-item
 	globalSizes := []int{1 << 12, 1 << 14, 1 << 16, 1 << 18, 1 << 20}
 	maxGlobal := 1 << 20
-	outBuf := C.clCreateBuffer(context, C.CL_MEM_WRITE_ONLY, C.size_t(maxGlobal*8), nil, &err)
+	outBuf := C.clCreateBuffer(context, C.CL_MEM_WRITE_ONLY, C.size_t(maxGlobal*4), nil, &err)
 	check(err, "clCreateBuffer")
 	defer C.clReleaseMemObject(outBuf)
 
-	iterArg := C.cl_ulong(iterations)
+	iterArg := C.cl_float(iterations)
 	err = C.clSetKernelArg(kernel, 1, C.size_t(unsafe.Sizeof(iterArg)), unsafe.Pointer(&iterArg))
 	check(err, "clSetKernelArg(iter)")
 
